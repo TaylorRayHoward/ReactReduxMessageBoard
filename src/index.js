c4dc9af18d4967e00655f86031611df2223009cf
Taylor Howard <taylorhoward10@gmail.com>
2017-08-08 22:50:53 -0500

    Add react-router-dom

--- src/index.js
+++ src/index.js
@@ -6,12 +6,17 @@ import { applyMiddleware, createStore } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from './Reducers/index';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 const createStoreWithMiddleware = applyMiddleware(thunk)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <ListPosts />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={ListPosts}/>
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'));
 registerServiceWorker();
