57c26c0bbacdc3e4178d79b6fb96dbab40a052c9
Taylor Howard <taylorhoward10@gmail.com>
2017-08-13 17:47:37 -0500

    Bad merge

--- src/Components/InputField.js
+++ src/Components/InputField.js
@@ -1,12 +1,10 @@
 /**
- * Created by taylorrayhoward on 8/13/17.
+ * Created by taylorrayhoward on 8/10/17.
  */
 import React from 'react';
-
 function isVowel(char) {
   return /^[aeiou]$/.test(char.toLowerCase());
 }
-
 const InputField = (props) => {
   return (
     <div className="form-group row">
@@ -14,12 +12,9 @@ const InputField = (props) => {
       <div className="col-sm-10">
         <input onChange={props.inputAction} type={props.type}
                id={props.id}
-               className="form-control"
-               placeholder={`Please enter ${isVowel(props.label[0]) ? 'an' : 'a'} ${props.label}`}
-               style={props.style}/>
+               className="form-control" placeholder={`Please enter ${isVowel(props.label[0]) ? "an" : "a"} ${props.label}...`} style={props.style}/>
       </div>
     </div>
   );
 };
-
 export default InputField;
