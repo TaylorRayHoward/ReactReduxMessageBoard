6b206046a9ee26db547e9872c54ea47acd05e6dc
Taylor Howard <taylorhoward10@gmail.com>
2017-08-17 23:38:37 -0500

    Missed import

--- src/Components/SocialMediaLogin.js
+++ src/Components/SocialMediaLogin.js
@@ -8,12 +8,14 @@ const SocialMediaLogin = (props) => {
   const { googleLogin, twitterLogin } = props;
   return (
     <div className="d-flex justify-content-between mt-1">
-      <a href="#" className="btn btn-social btn-google" onClick={googleLogin} >
+      <a href="#" className="btn btn-social btn-google" onClick={googleLogin}>
         <span className="fa fa-google"/> Sign in with Google
       </a>
-      <a href="#" className="btn btn-social btn-twitter" onClick={twitterLogin} >
+      <a href="#" className="btn btn-social btn-twitter" onClick={twitterLogin}>
         <span className="fa fa-twitter"/> Sign in with Twitter
       </a>
     </div>
   );
 };
+
+export default SocialMediaLogin;
